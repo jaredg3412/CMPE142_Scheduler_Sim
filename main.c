cd4698de3968cec5a704f03f8eb8d623b65c950c
jaredg3412 <jared.arroyogarcia@gmail.com>
2019-04-14 00:28:31 -0700

    Added parsing functionality to parse jobs.dat file and echo what is read for testing purposes

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main (){
 	char echo;
 	char file_name [32] = "jobs.dat";
 	FILE *f;
+
+	//needed to parse input file 
+	char buffer[32];
+	int integers[300];
+	
+	int j = 0;
+	int i = 0;
 
 	f = fopen(file_name, "r");
 
@@ -13,8 +21,26 @@ int main (){
 		perror("Cannot open file. \n");
 	}
 	
-	while((echo = fgetc(f)) != EOF)
-		printf("%c", echo);
+	while((echo = fgetc(f)) != EOF){
+		if(echo == ' ' || echo == '\n' )
+		{
+			integers[i] = atoi(buffer);
+			i++;
+			bzero(buffer,32);
+			j=0;
+		}
+		else
+		{
+			buffer[j] = echo;
+			j++;
+		}
+	}
+	i = 0;
+	while(i<9)
+	{
+		printf("integers %d \n", integers[i]);
+		i++;
+	}
 
 	fclose(f);
 	return 0;
